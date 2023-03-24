362025a1bf94398852549cd266a9bb5c43fdfc51
rmoin <41532338+rmoin@users.noreply.github.com>
2023-03-23 20:59:36 -0700

    Update Home.jsx

--- src/frontend/src/App/Home.jsx
+++ src/frontend/src/App/Home.jsx
@@ -30,26 +30,17 @@ export default function Home(props) {
     return (
         <div style={{ paddingLeft: "0px", paddingTop: "50px" }}>
             <Text weight="semibold" style={{ fontSize: "25px", display: "block", marginBottom: "20px" }}>Turigma Turbo</Text>
-            <Text style={{ display: "block", marginBottom: "10px" }}>
-                Build multi-stage AI/ML pipelines across AI and ML Services using pretrained, or your own custom/open source models in our Studio. Create a new pipeline by selecting “Configure a New Pipeline” below. After the input file type is selected, new Services will appear within your pipeline builder from (Azure Language, Form Recognizer, Speech, etc..), based on the file type (e.g. audio files). As files are transformed or converted, from images or audio to text, new Services will become available to be added to the pipeline builder. For example, Language Services options for data that has been transformed to text.
-            </Text>
-            <Text style={{ display: "block", marginBottom: "10px" }}>
-                As you start to build longer pipelines, with multiple transformations, you can optionally continue building the pipeline against data in an earlier stage within the pipeline, with the “Change Output” card.
-            </Text>
-            <Text style={{ display: "block", marginBottom: "40px" }}>
-                Once the pipeline is built, a cost estimate can be generated. And the pipeline can will be triggered once documents are uploaded.
-            </Text>
 
 
-            <Text weight="semibold" style={{ fontSize: "18px", display: "block", marginBottom: "20px" }}>What would you like to do?</Text>
+            <Text weight="semibold" style={{ fontSize: "18px", display: "block", marginBottom: "20px" }}>Set up your workflow</Text>
             {/* <div style={{display:"flex", paddingTop : "50px"}}> */}
-            <Button id="CONFIGURE_PIPELINE" onClick={(e) => props.onClick(e)} text style={{ color: "rgb(0, 120, 212)", paddingLeft: "0px" }} content="Configure A New Pipeline" />
+            <Button id="CONFIGURE_PIPELINE" onClick={(e) => props.onClick(e)} text style={{ color: "rgb(0, 120, 212)", paddingLeft: "0px" }} content="Configure A New Workflow" />
             |
-            <Button id="CURRENT_PIPELINE" onClick={(e) => props.onClick(e)} text style={{ color: "rgb(0, 120, 212)" }} content="View The Existing Pipeline " />
+            <Button id="CURRENT_PIPELINE" onClick={(e) => props.onClick(e)} text style={{ color: "rgb(0, 120, 212)" }} content="View The Existing Workflow " />
             |
-            <Button id="UPLOAD_DOCUMENTS" onClick={(e) => props.onClick(e)} text style={{ color: "rgb(0, 120, 212)" }} content="Ingest Documents" />
+            <Button id="UPLOAD_DOCUMENTS" onClick={(e) => props.onClick(e)} text style={{ color: "rgb(0, 120, 212)" }} content="Upload Documents" />
             |
-            <Button id="VIEW_INSIGHTS" onClick={(e) => props.onClick(e)} text style={{ color: "rgb(0, 120, 212)" }} content="Sample Search Application" />
+            <Button id="VIEW_INSIGHTS" onClick={(e) => props.onClick(e)} text style={{ color: "rgb(0, 120, 212)" }} content="Search Your Data" />
             {renderOAIOptions()}
 
             {/* </div> */}
